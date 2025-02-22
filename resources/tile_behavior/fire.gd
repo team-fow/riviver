8dbf63195cdc7ddffc38f4a08fd88914fc46fe3d
webcatzz <71499717+webcatzz@users.noreply.github.com>
2025-02-22 03:27:17 -0500

    cooler firespread

--- resources/tile_behavior/fire.gd
+++ resources/tile_behavior/fire.gd
@@ -4,6 +4,11 @@ extends TileBehavior
 func tick() -> void:
 	tile.type = Tile.Type.ASH
 	
-	for neighbor: Tile in tile.get_neighbors():
-		if neighbor.get_info().flammable:
-			neighbor.type = Tile.Type.FIRE
+	var neighbors: Array[Tile] = tile.get_neighbors().filter(_is_tile_flammable)
+	if neighbors:
+		for i: int in randi_range(1, 2):
+			neighbors.pick_random().type = Tile.Type.FIRE
+
+
+func _is_tile_flammable(tile: Tile) -> bool:
+	return tile.get_info().flammable
