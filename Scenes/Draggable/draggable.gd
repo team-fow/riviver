ea9bde5612b8f6b6ba6c37163d740567dc3ee553
webcatzz <71499717+webcatzz@users.noreply.github.com>
2025-03-26 17:23:48 -0400

    added signals + virtuals to draggable

--- Scenes/Draggable/draggable.gd
+++ Scenes/Draggable/draggable.gd
@@ -1,6 +1,9 @@
 class_name Draggable # The base class for draggable nodes - requires a collision shape as a child node 
 extends Area2D
 
+signal grabbed
+signal dropped
+
 var held: bool = false
 var hovered: bool = false
 const HOVERED_SCALE := Vector2(1.25, 1.25)
@@ -13,12 +16,24 @@ func _unhandled_input(event: InputEvent) -> void:
 			global_position = get_global_mouse_position()
 		elif event.is_action_released("click"):
 			held = false
+			dropped.emit()
+			_drop()
 	else:
 		if event.is_action_pressed("click") && hovered:
 			held = true
+			grabbed.emit()
+			_grab()
+
+
+
+# custom virtual functions
+
+func _grab() -> void: pass
+func _drop() -> void: pass
 
 
 # Virtual
+
 func _ready() -> void:
 	mouse_entered.connect(_on_mouse_entered)
 	mouse_exited.connect(_on_mouse_exited)
