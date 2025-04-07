45fa2f8921e49e969646a222cc2e7e0bf6e70a57
webcatzz <71499717+webcatzz@users.noreply.github.com>
2025-04-06 21:11:17 -0400

    progress bar coloring

--- Scenes/levels/base/progress_bar_part.gd
+++ Scenes/levels/base/progress_bar_part.gd
@@ -12,7 +12,7 @@ func set_sprite(value: Sprite) -> void:
 	var sprites = [
 		load("res://assets/progressbar/Progress - Not Completed.png"),
 		load("res://assets/progressbar/Progress - Bad.png"),
-		load("res://assets/progressbar/Progress - Medium.png"),
 		load("res://assets/progressbar/Progress - Good.png"),
+		load("res://assets/progressbar/Progress - Medium.png"),
 	]
 	texture = sprites[value]
