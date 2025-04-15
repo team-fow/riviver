fdc787f0d58f8471e4158ce6196ef0c2b1f16695
webcatzz <71499717+webcatzz@users.noreply.github.com>
2025-04-15 16:44:20 -0400

    trash scale + default font

--- Scenes/minigames/trash_minigame/trash_object.gd
+++ Scenes/minigames/trash_minigame/trash_object.gd
@@ -5,6 +5,8 @@ signal eaten
 
 @export_enum("landfill", "recycling", "compost", "hazardous") var category: String = "landfill"
 
+var initial_scale: Vector2i = scale
+
 
 func _drop() -> void:
 	for area: Area2D in get_overlapping_areas():
@@ -12,6 +14,14 @@ func _drop() -> void:
 			area.eat(self)
 			eaten.emit()
 			queue_free()
+
+
+func _mouse_enter() -> void:
+	scale = initial_scale * 1.1
+
+
+func _mouse_exit() -> void:
+	scale = initial_scale
 
 
 func _ready() -> void:
