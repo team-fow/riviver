79e0bcc017b7e61c51ef6debeea2b3d17e7fa576
webcatzz <71499717+webcatzz@users.noreply.github.com>
2025-04-17 13:01:00 -0400

    worldmap tweaks

--- Scenes/worldmap/worldmap.gd
+++ Scenes/worldmap/worldmap.gd
@@ -81,6 +81,8 @@ func _on_settings_pressed() -> void:
 
 
 func do_intro_cutscene() -> void:
+	$PollutedMap.modulate = Color.BLACK
+	$UI/Margins.modulate.a = 0.0
 	level_info.hide()
 	scienceguy.show()
 	scienceguy.set_sprite(scienceguy.Sprite.HAPPY)
