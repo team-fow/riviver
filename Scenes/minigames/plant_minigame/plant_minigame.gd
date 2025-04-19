de98b0a1ea0b9fec9e650d016768ef8667d02a98
sagealtman <jaltman01@wesleyan.edu>
2025-04-19 14:04:12 -0400

    plant minigame end animation

--- Scenes/minigames/plant_minigame/plant_minigame.gd
+++ Scenes/minigames/plant_minigame/plant_minigame.gd
@@ -11,7 +11,7 @@ var held_tool: Tool
 var plants: Array[Area2D]
 
 @onready var background: Control = $Background
-@onready var water: TextureRect = $Background/PanelContainer/Water
+@onready var water: TextureRect = $Background/Water
 @onready var timer_label: Label = $Background/TimerLabel
 @onready var timer: Timer = $Timer
 @onready var animator: AnimationPlayer = $Animator
@@ -29,7 +29,7 @@ func _ready() -> void:
 		plants.append(plant)
 		$Plants.add_child(plant)
 	$Plants.position.x = (plant_count - 1) * PLANT_SEPARATION * -0.5
-
+	
 
 func _on_plant_grown() -> void:
 	plants_grown += 1
@@ -42,14 +42,16 @@ func end() -> void:
 		total_score += float(plant.state) / PlantMinigamePlant.State.WATERED
 	score = total_score / plants.size()
 	
-	if score < 0.75:
-		water.texture = preload("res://assets/minigames/plant/eroded_river_foreground.png")
-		water.z_index = 1
-	
 	animator.play("water")
 	await animator.animation_finished
 	ended.emit(self, score)
 
+
+func change_texture() -> void:
+	if score < 0.75:
+		water.texture = preload("res://assets/minigames/plant/eroded_river_foreground.png")
+		water.pivot_offset = Vector2(500, 434.27)
+		water.z_index = 1
 
 
 # timer
