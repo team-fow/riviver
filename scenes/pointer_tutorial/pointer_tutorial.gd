c9bc30dadbcdd7490c20fe378617905ebe3d5b5a
sagealtman <jaltman01@wesleyan.edu>
2025-04-19 19:42:29 -0400

    pointer tutorial framework
    will add specific functionality soon

---
+++ scenes/pointer_tutorial/pointer_tutorial.gd
@@ -0,0 +1,17 @@
+extends Sprite2D
+
+var tween: Tween
+
+
+func tutorial_point(pos_a: Vector2, pos_b: Vector2) -> void:
+	global_position = pos_a
+	
+	tween = get_tree().create_tween()
+	tween.tween_property(self, "modulate", Color.WHITE, 0.1)
+	await tween.finished
+	
+	tween.tween_property(self, "global_position", pos_b, 0.25)
+	await tween.finished
+	
+	tween.tween_property(self, "modulate", Color(1.0,1.0,1.0,0), 0.1)
+	await tween.finished
