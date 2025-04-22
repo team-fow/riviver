1cd6b53ac5ea17e00847bd3a45d051f5504e48d5
sagealtman <jaltman01@wesleyan.edu>
2025-04-22 11:53:22 -0400

    removed extra print statements

--- scenes/pointer_tutorial/pointer_tutorial.gd
+++ scenes/pointer_tutorial/pointer_tutorial.gd
@@ -10,14 +10,11 @@ func tutorial_point(pos_a: Vector2, pos_b: Vector2) -> void:
 	tween = get_tree().create_tween()
 	tween.tween_property(self, "modulate", Color.WHITE, 0.1)
 	await tween.finished
-	print("a")
 	
 	tween = get_tree().create_tween()
 	tween.tween_property(self, "global_position", pos_b, 1)
 	await tween.finished
-	print("b")
 	
 	tween = get_tree().create_tween()
 	tween.tween_property(self, "modulate", Color(1.0,1.0,1.0,0.0), 0.1)
 	await tween.finished
-	print("c")
