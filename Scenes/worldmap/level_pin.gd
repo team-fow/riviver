4ba697194ada7b61717a5ce09451e633e0a185a3
webcatzz <71499717+webcatzz@users.noreply.github.com>
2025-04-26 20:26:23 -0400

    level pins consume input (no particles)

--- Scenes/worldmap/level_pin.gd
+++ Scenes/worldmap/level_pin.gd
@@ -24,6 +24,7 @@ func _ready() -> void:
 func _input_event(viewport: Viewport, event: InputEvent, shape_idx: int) -> void:
 	if event.is_action_pressed("click"):
 		$"../../".select_level(get_index())
+		get_viewport().set_input_as_handled()
 
 
 func flick() -> void:
