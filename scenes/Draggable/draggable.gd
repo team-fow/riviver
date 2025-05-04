7445a0791a88b42a19bf0715f76e114a3dba14e2
sagealtman <jaltman01@wesleyan.edu>
2025-05-04 19:46:38 -0400

    Update draggable.gd

--- scenes/Draggable/draggable.gd
+++ scenes/Draggable/draggable.gd
@@ -4,28 +4,27 @@ extends Area2D
 signal grabbed
 signal dropped
 
-var held: bool = false
-var hovered: bool = false
-var curr_tween: Tween
+var held: bool = false # Whether the node is currently being dragged by the player (should be following mouse)
+var hovered: bool = false # Whether the mouse is within the bounds of the given collision shape
+var curr_tween: Tween # Keeps track of the node's active tween (used for the object swinging as it is dragged around)
 
 
 # Handles clicking and dragging
 func _unhandled_input(event: InputEvent) -> void:
 	if held:
-		if event is InputEventMouseMotion:
+		if event is InputEventMouseMotion: # Handles dragging the node around
 			global_position = get_global_mouse_position().clamp(Vector2(-900.0, -500.0), Vector2(900.0, 500.0))
-			print(get_viewport_rect())
 			var mouse_velocity_x: float = event.screen_velocity.x
 			if curr_tween: curr_tween.kill()
 			curr_tween = get_tree().create_tween()
-			curr_tween.tween_property(self, "rotation_degrees", mouse_velocity_x*9.0/100.0, 0.1)
-		elif event.is_action_released("click"):
+			curr_tween.tween_property(self, "rotation_degrees", (mouse_velocity_x * 9.0)/100.0, 0.1) # Causes dragged objects to swing with mouse input
+		elif event.is_action_released("click"): # Handles dropping the node
 			if curr_tween: curr_tween.kill()
 			curr_tween = get_tree().create_tween()
 			curr_tween.tween_property(self, "rotation_degrees", 0, 0.05)
 			drop()
 	else:
-		if event.is_action_pressed("click") && hovered:
+		if event.is_action_pressed("click") && hovered: # Handles clicking the node to pick it up
 			grab()
 			get_viewport().set_input_as_handled()
 
